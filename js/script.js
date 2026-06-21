d929d00a4202c4e75a06debeb2f8da95c2a887c5
Alyssa <francinalyssa@gmail.com>
2026-06-21 13:21:15 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
 
+console.log("Portfolio Loaded!");
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-link");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 100;
+        const sectionHeight = section.clientHeight;
+
+        if (window.scrollY >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === `#${current}`) {
+            link.classList.add("active");
+        }
+    });
+});
